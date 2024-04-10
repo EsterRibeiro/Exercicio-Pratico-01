27d724b88041919098327450a1af3deb68e45be7
EsterRibeiro <ester.ribeiro10101@gmail.com>
2024-04-09 23:21:19 -0300

    exercicio pratico lista 1

---
+++ Pratica_Exercicio7/Program.cs
@@ -0,0 +1,36 @@
+﻿using Pratica_Exercicio7.Entities;
+using Pratica_Exercicio7.Repositories;
+
+var funcionario = new Funcionario();
+funcionario.Id = Guid.NewGuid();
+
+Console.WriteLine("Informe os dados do funcionário:\n");
+
+Console.Write("Nome: ");
+funcionario.Nome = Console.ReadLine();
+
+Console.Write("CPF: ");
+funcionario.Cpf = Console.ReadLine();
+
+Console.Write("Matrícula: ");
+funcionario.Matricula = Console.ReadLine();
+
+Console.Write("Data de admissão: ");
+funcionario.DataAdmissao = DateTime.Parse(Console.ReadLine());
+
+Console.Write("Cargo: ");
+funcionario.Cargo = Console.ReadLine();
+
+Console.Write("Salário: ");
+funcionario.Salario = decimal.Parse(Console.ReadLine());
+
+var funcionarioRepository = new FuncionarioRepository();
+
+funcionarioRepository.ExportarDados(funcionario);
+
+Console.WriteLine("Dados do funcionário\n");
+Console.WriteLine($"\nCpf: {funcionario.Cpf}");
+Console.WriteLine($"\nMatrícula: {funcionario.Matricula}");
+Console.WriteLine($"\nData de admissão: {funcionario.DataAdmissao}");
+Console.WriteLine($"\nCargo: {funcionario.Cargo}");
+Console.WriteLine($"\nSalario: {funcionario.Salario}");
---
+++ Pratica_Exercicio7/Repositories/FuncionarioRepository.cs
@@ -0,0 +1,19 @@
+﻿using Pratica_Exercicio7.Entities;
+
+namespace Pratica_Exercicio7.Repositories;
+public class FuncionarioRepository
+{
+    public void ExportarDados(Funcionario funcionario)
+    {
+        var streamWriter = new StreamWriter(@"c:\temp\funcionario.txt", true);
+
+        streamWriter.WriteLine($"Nome: { funcionario.Nome }");
+        streamWriter.WriteLine($"Cpf: {funcionario.Cpf}");
+        streamWriter.WriteLine($"Matrícula: {funcionario.Matricula}");
+        streamWriter.WriteLine($"Data de admissão: {funcionario.DataAdmissao}");
+        streamWriter.WriteLine($"Cargo: {funcionario.Cargo}");
+        streamWriter.WriteLine($"Salario: {funcionario.Salario}");
+
+        streamWriter.Close();
+    }
+}
